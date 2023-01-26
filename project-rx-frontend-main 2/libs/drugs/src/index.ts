4309d0fcdc0305a7f5151e832c99bb9b74aa24e6
Yahan Deshapriya <yahandeshapriya@gmail.com>
2023-01-26 09:33:10 +0530

    update files

--- project-rx-frontend-main 2/libs/drugs/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './lib/drugs.module';
-export * from './lib/services/drugs.service'
-export * from './lib/models/drug'
--- project-rx-frontend-main 2/libs/drugs/src/lib/models/drug.ts
+++
@@ -1,7 +0,0 @@
-export class Drug {
-    _id? : string;
-    generic_name? : string;
-    brand_name? : string;
-    dosage_form? : string;
-    manufacturer? : string;
-}
